8f6c90594ed546def09f5a5488541cfa2a75783b
omerakdogan <omerakdogan@icloud.com>
2021-07-10 00:25:15 +0000

    hello

---
+++ hello.js
@@ -0,0 +1,10 @@
+console.log("Hello, Caliban");
+console.log("Hello, Miranda");
+console.log("Hello, Ferdinand");
+
+const sayHello  = function () {
+  console.log("Hello, world");
+}
+
+sayHello();
+
